d585283406355e8106c337705f2a72e2b46b7165
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2019-07-10 21:26:44 +0300

    add education and experience

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,8 @@ import Dashboard from "./components/dashboard/Dashboard";
 import PrivateRoute from "./components/routing/PrivateRoute";
 import CreateProfile from "./components/profile-forms/CreateProfile";
 import EditProfile from "./components/profile-forms/EditProfile";
+import AddExperience from "./components/profile-forms/AddExperience";
+import AddEducation from "./components/profile-forms/AddEducation";
 // Redux
 import { Provider } from "react-redux";
 import store from "./store";
@@ -36,6 +38,7 @@ const App = () => {
             <Switch>
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
+              <PrivateRoute exact path="/dashboard" component={Dashboard} />
               <PrivateRoute
                 exact
                 path="/create-profile"
@@ -46,7 +49,16 @@ const App = () => {
                 path="/edit-profile"
                 component={EditProfile}
               />
-              <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              <PrivateRoute
+                exact
+                path="/add-experience"
+                component={AddExperience}
+              />
+              <PrivateRoute
+                exact
+                path="/add-education"
+                component={AddEducation}
+              />
             </Switch>
           </section>
         </Fragment>
