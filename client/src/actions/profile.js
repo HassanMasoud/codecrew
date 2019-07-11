048fa630311343913550420ba3225f208817e2db
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2019-07-11 16:19:02 +0300

    started on profile component

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -13,7 +13,7 @@ import {
 // get current users profile
 export const getCurrentProfile = () => async dispatch => {
   try {
-    const res = await axios.get("api/profile/me");
+    const res = await axios.get("/api/profile/me");
 
     dispatch({
       type: GET_PROFILE,
@@ -31,7 +31,7 @@ export const getCurrentProfile = () => async dispatch => {
 export const getProfiles = () => async dispatch => {
   dispatch({ type: CLEAR_PROFILE });
   try {
-    const res = await axios.get("api/profile");
+    const res = await axios.get("/api/profile");
 
     dispatch({
       type: GET_PROFILES,
@@ -46,9 +46,9 @@ export const getProfiles = () => async dispatch => {
 };
 
 // Get profile by ID
-export const getProfile = userId => async dispatch => {
+export const getProfileById = userId => async dispatch => {
   try {
-    const res = await axios.get(`api/profile/user/${userId}`);
+    const res = await axios.get(`/api/profile/user/${userId}`);
 
     dispatch({
       type: GET_PROFILE,
@@ -65,7 +65,7 @@ export const getProfile = userId => async dispatch => {
 // Get github repos
 export const getGithubRepos = username => async dispatch => {
   try {
-    const res = await axios.get(`api/profile/github/${username}`);
+    const res = await axios.get(`/api/profile/github/${username}`);
 
     dispatch({
       type: GET_REPOS,
@@ -226,7 +226,7 @@ export const deleteEducation = id => async dispatch => {
 export const deleteAccount = () => async dispatch => {
   if (window.confirm("Are you sure? This can NOT be undone")) {
     try {
-      const res = await axios.delete("/api/profile");
+      await axios.delete("/api/profile");
 
       dispatch({ type: CLEAR_PROFILE });
       dispatch({ type: ACCOUNT_DELETED });
