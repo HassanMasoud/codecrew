dfd37cd9b66744bdfbe3d1e48febafd7986731fd
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2019-07-17 14:27:13 +0300

    added styling

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -15,13 +15,11 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
       </li>
       <li>
         <Link to="/dashboard">
-          <i className="fas fa-user" />{" "}
           <span className="hide-sm">Dashboard</span>
         </Link>
       </li>
       <li>
         <a onClick={logout} href="#!">
-          <i className="fas fa-sign-out-alt" />{" "}
           <span className="hide-sm">Logout</span>
         </a>
       </li>
@@ -46,7 +44,7 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
     <nav className="navbar bg-dark">
       <h1>
         <Link to="/">
-          <i className="fas fa-code" /> DevConnector
+          <i className="fas fa-code" /> CodeCrew
         </Link>
       </h1>
       {!loading && (
